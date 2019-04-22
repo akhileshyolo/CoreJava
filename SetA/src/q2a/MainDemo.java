ccd73b693049189b918022506067f4afd9f539e6
akhilesh <aksyvakin@gmail.com>
2019-04-22 15:51:07 +0530

    Second Push

--- SetA/src/q2a/MainDemo.java
+++ SetA/src/q2a/MainDemo.java
@@ -1,11 +1,29 @@
 package q2a;
 
+import java.util.*;
+
 public class MainDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//Mango[5] mangoes = new Mango();
+		//Integer num[] = new Integer[10];
+		Mango mangoes[] = new Mango[5];
+		
+		mangoes[0] = new Mango("Kesar",7,700);
+		mangoes[1] = new Mango("Badami",6,800);
+		mangoes[2] = new Mango("Alphonso",10,100);
+		mangoes[3] = new Mango("Totapuri",1,100);
+		mangoes[4] = new Mango("Dasheri",5,100);
+		
+		List<Mango> mangolist = new ArrayList<Mango>();
+		mangolist.addAll([mangoes[0], mangoes[1], mangoes[2], mangoes[3],mangoes[4]]);
+		
+		
+		
+		
+		
+		
 		
 	}
 
